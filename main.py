b9b1f599bf3b2a0076e9d79e29913ac9da69044f
Antonio <antonio_prieto_jimenez@hotmail.com>
2015-12-27 19:29:18 +0100

    Mapa de España Conversión de datos de Latitud y Longitud de Madrid Dibujo de un punto marcando Madrid

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 
-map = Basemap(llcrnrlon=3.75,llcrnrlat=39.75,urcrnrlon=4.35,urcrnrlat=40.15, epsg=5520)
+map = Basemap(llcrnrlon=-10.5,llcrnrlat=33,urcrnrlon=10,urcrnrlat=46, epsg=5520)
 #http://server.arcgisonline.com/arcgis/rest/services
 
 map.arcgisimage(service='ESRI_StreetMap_World_2D', xpixels = 1500, verbose= True)
 
-x, y = map(3.8, 40)  # Obtiene las coordenadas para dibujar a partir de latitud y longitud
-map.plot(x, y, 'bo', markersize = 24)
+x, y = map(-3.7037901999999576, 40.4167754)  # Obtiene las coordenadas de Madrid para dibujar a partir de latitud y longitud
+map.plot(x, y, 'bo', markersize = 10)
 
 plt.show()
+
